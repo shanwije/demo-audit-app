709e74cd7c94b8e736ab02304250837dc62f1c8d
Shan Wijenayaka <shanwije.dev@gmail.com>
2023-05-12 13:59:56 +0800

    init-commit

---
+++ src/main/java/com/shanwije/auditapp/AuditAppApplication.java
@@ -0,0 +1,13 @@
+package com.shanwije.auditapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shanwije/auditapp/AuditAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanwije.auditapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
